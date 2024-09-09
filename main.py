5bb825e0c0563ce4d881abfe4ad3a638be81473b
Axel Padilla <adpadillar25@gmail.com>
2024-09-09 08:55:52 -0600

    circle implementation

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ Exercises
 
 from turtle import *
 from freegames import vector
+import math
 
 
 def line(start, end):
@@ -35,10 +36,16 @@ def square(start, end):
     end_fill()
 
 
-def circle(start, end):
-    """Draw circle from start to end."""
-    pass  # TODO 
-
+def draw_circle(start, end):
+    """Calculate radius based on distance between start and end"""
+    radius = math.sqrt((end.x - start.x)**2 + (end.y - start.y)**2)
+    
+    up()
+    goto(start.x, start.y - radius)  # Move to the bottom of the circle
+    down()
+    begin_fill()
+    circle(radius)  # Draw the circle
+    end_fill()
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
@@ -81,7 +88,7 @@ onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', draw_circle), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
