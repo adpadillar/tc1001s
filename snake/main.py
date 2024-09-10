cdd6bbe4276744324c895cf5b8978b51dc87a9c2
Axel Padilla <adpadillar25@gmail.com>
2024-09-10 09:01:54 -0600

    make snake go faster when it eats food

--- snake/main.py
+++ snake/main.py
@@ -5,6 +5,7 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+speed = 100  # Initial speed
 
 def change(x, y):
     "Change snake direction."
@@ -25,6 +26,7 @@ snakeColor, foodColor = sample(colors, 2)
 
 def move():
     "Move snake forward one segment."
+    global speed  # Add global speed variable
     head = snake[-1].copy()
     head.move(aim)
 
@@ -39,6 +41,7 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+        speed = max(50, speed - 2) 
     else:
         newFood = {"x": -1000, "y": -1000}
         while not inside_food(newFood):
@@ -56,7 +59,7 @@ def move():
 
     square(food.x, food.y, 9, foodColor)
     update()
-    ontimer(move, 100)
+    ontimer(move, speed)
 
 setup(420, 420, 370, 0)
 hideturtle()
@@ -68,4 +71,3 @@ onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
 done()
-
